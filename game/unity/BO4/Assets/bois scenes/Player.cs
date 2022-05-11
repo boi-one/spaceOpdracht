000753167d50de2d90b164fb17a94004cc19babc
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2022-05-11 16:43:11 +0200

    ben bezig met de schiet

--- game/unity/BO4/Assets/bois scenes/Player.cs
+++ game/unity/BO4/Assets/bois scenes/Player.cs
@@ -16,17 +16,19 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) && transform.position.y < -1.5)
         {
-            transform.position += new Vector3(0, speed) * Time.deltaTime;   
+            transform.position += new Vector3(0, speed) * Time.deltaTime;
+            transform.localScale -= (new Vector3(speed / 1.4f, speed) * Time.deltaTime) /3;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -8.36)
         {
             transform.position -= new Vector3(speed, 0) * Time.deltaTime;   
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -4.02)
         {
             transform.position -= new Vector3(0, speed) * Time.deltaTime;
+            transform.localScale += (new Vector3(speed / 1.4f, speed) * Time.deltaTime) /3;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 8.42)
         {
             transform.position += new Vector3(speed, 0) * Time.deltaTime;
         }
