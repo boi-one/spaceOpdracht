3f0075874105b14c9abe629fbb661779c957d30f
unknown <30570@ma-web.nl>
2022-05-12 11:00:46 +0200

    bezig met items

--- game/unity/BO4/Assets/bois scenes/Player.cs
+++ game/unity/BO4/Assets/bois scenes/Player.cs
@@ -5,38 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public float Scale1 = 1.4f;
+    public float Scale1 = 4f;
     public float Scale2 = 1f;
+    public List<GameObject> gameitems;
+    public int[] gameitemsarray = new int[] {};
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && transform.position.y < -1.5)
+        if (Input.GetKey(KeyCode.W) && transform.position.y < -3)
         {
             transform.position += new Vector3(0, speed) * Time.deltaTime;
             transform.localScale -= (new Vector3(speed / Scale1, speed / Scale2) * Time.deltaTime) /3;
         }
-        if (Input.GetKey(KeyCode.A) && transform.position.x > -8.36)
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -6.5)
         {
             transform.position -= new Vector3(speed, 0) * Time.deltaTime;   
         }
-        if (Input.GetKey(KeyCode.S) && transform.position.y > -4.3)
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -5)
         {
             transform.position -= new Vector3(0, speed) * Time.deltaTime;
             transform.localScale += (new Vector3(speed / 1.4f, speed) * Time.deltaTime) /3;
         }
-        if (Input.GetKey(KeyCode.D) && transform.position.x < 8.42)
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 5)
         {
             transform.position += new Vector3(speed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.Space))
         {
-
+            
         }
     }
 }
